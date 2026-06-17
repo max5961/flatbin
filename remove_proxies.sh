ad39616a6aeb19cf1bcb831f91c12e7252da129e
Maxwell Morrissey <mmorrissey92@proton.me>
2026-06-16 23:23:37 -0400

    Better cleanup for proxy removal

--- remove_proxies.sh
+++ remove_proxies.sh
@@ -2,9 +2,23 @@
 
 source ./constants.sh
 
+remaining=
+removed_count=0
 while IFS= read -r proxy_file; do
     if grep -E "^${SYMBOL}\$" "${proxy_file}" >/dev/null 2>&1; then
         echo "Removing ${proxy_file}"
         sudo rm "${proxy_file}"
+        ((++removed_count))
+    else
+        remaining+="${proxy_file} "
     fi
 done < "${PROXIES}"
+
+printf '' > "${PROXIES}"
+for file in $remaining; do
+    echo "${file}" >> "${PROXIES}"
+done
+
+if [[ "$removed_count" == 0 ]]; then
+    echo "Nothing to remove"
+fi
