7cc486c0ddb4ebacde9fcd41a7ef668e6b97ee64
Maxwell Morrissey <mmorrissey92@proton.me>
2026-06-27 18:58:21 -0400

    fix-bug: more robust app id retrieval

--- remove_proxies.sh
+++ remove_proxies.sh
@@ -5,6 +5,11 @@ source ./constants.sh
 remaining=
 removed_count=0
 while IFS= read -r proxy_file; do
+    # Need to first create a list of installed apps by name, then verify that flatpak
+    # still has them installed in order to only remove the uninstalled apps...or
+    # if the flags are telling the script to remove everything, then remove everything
+    # regardless
+
     if grep -E "^${SYMBOL}\$" "${proxy_file}" >/dev/null 2>&1; then
         echo "Removing ${proxy_file}"
         sudo rm "${proxy_file}"
